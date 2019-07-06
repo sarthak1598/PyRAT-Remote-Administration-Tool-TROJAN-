4f2e91fa8e1833dc549ca912dcb7e4a74e5ece15
Sarthak Gupta <789sk.gupta@gmail.com>
2019-07-07 01:42:24 +0530

    Create makeexefile.py

---
+++ makeexefile.py
@@ -0,0 +1,5 @@
+
+
+# this is a  one liner command to be executed on the linux terminal to convert the python .py file to the respective binary/ELF executable file
+
+>> pyinstaller --onefile <name-of-script.py>
